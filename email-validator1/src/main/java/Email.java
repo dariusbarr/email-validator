e1ced37f5b1149d1ffeb78046494690cad0dd1a5
dariusbarr <dr294786@dal.ca>
2017-03-01 21:11:52 -0400

    Part 1

---
+++ email-validator1/src/main/java/Email.java
@@ -0,0 +1,52 @@
+
+public class Email {
+
+	String email;
+	
+	public Email(String email)
+	{
+		this.email=email;
+	}
+	
+	public boolean atSign()
+	{
+		boolean result=false;
+		for(int i=0;i<email.length();i++)
+		{
+			if(email.charAt(i)=='@')
+			{
+				result=true;
+				
+				break;
+			}
+			
+		}
+		
+		if(result==false)
+		System.out.println("@ sign not found");
+		
+		else System.out.println("@ sign found");
+		return result;
+	}
+	
+	public boolean period()
+	{
+		boolean result=false;
+		for(int i=0;i<email.length();i++)
+		{
+			if(email.charAt(i)=='.')
+			{
+				result=true;
+				
+				break;
+			}
+			
+		}
+		
+		if(result==false)
+		System.out.println("'.' sign not found");
+		
+		else System.out.println("'.' sign found");
+		return result;
+	}
+}
